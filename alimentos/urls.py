465b915b08f4070782db3e8af3f1a3457c8cdf39
I.S.C. Javi Puc <angelpuc08@gmail.com>
2016-02-11 18:01:28 -0600

    Se elimino el css de nuevo_alimento y se modifico el name de la ruta hello a home

--- alimentos/urls.py
+++ alimentos/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
 	url(r'^login/$', views.auth_login, name='authentication'),
 	url(r'^logout$', auth_views.logout, {'next_page': '/'}, name='logout'),
-	url(r'^$', views.ListaAlimentos.as_view(), name = 'hello'),
+	url(r'^$', views.ListaAlimentos.as_view(), name = 'home'),
  	url(r'^alimento/(?P<pk>[0-9]+)/$', views.DetalleAlimento.as_view(), name='detalle'),
     url(r'^alimento/nuevo', views.nuevo_alimento, name="nuevo")
 ]
